48c7c994ce95c52cbb7cb85cc5d211d42257ae8b
ElRuso78 <43108109+DaniAndries@users.noreply.github.com>
2024-12-03 10:16:51 +0100

    03/12/24

--- src/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"controller.db"},{"l":"controller.db.impl"},{"l":"model"},{"l":"src"},{"l":"test.java"},{"l":"utils"}];updateSearchResults();
